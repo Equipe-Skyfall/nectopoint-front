92e00ecb60535a5deaf3ad1749aad82ccc23e162
victordanielrb <victordanielramosb@gmail.com>
2025-03-06 11:32:17 -0300

    [style] Adiciona fonte poppins

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': '/src', // Alias para facilitar a importação de arquivos em src
+    },
+  },
+  build: {
+    assetsDir: 'assets', // Diretório onde os assets serão copiados no build
+  },
+  publicDir: 'public', // Diretório para arquivos estáticos (opcional)
+});
