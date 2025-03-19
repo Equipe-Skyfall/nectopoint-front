20e9f4678de9a5df704b91e5194f7e2417c3b010
Eduardo Fontes <160733714+DuuhZero@users.noreply.github.com>
2025-03-19 01:01:21 -0300

    [feat]: Vite.config para porta 3000

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -12,4 +12,14 @@ export default defineConfig({
     assetsDir: 'assets', // Diretório onde os assets serão copiados no build
   },
   publicDir: 'public', // Diretório para arquivos estáticos (opcional)
+  server: {
+    port: 3000,
+    proxy: {
+      '/usuario': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 });
