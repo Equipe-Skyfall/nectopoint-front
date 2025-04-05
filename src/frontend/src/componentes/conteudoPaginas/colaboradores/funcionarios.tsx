cd090946840ec0da5654ad548f0d0cddb68505be
pauloalmeida46 <paulotober14@gmail.com>
2025-04-05 19:56:15 -0300

    fix: modificacao nos campos de validacao de colaboradores

--- src/frontend/src/componentes/conteudoPaginas/colaboradores/funcionarios.tsx
+++ src/frontend/src/componentes/conteudoPaginas/colaboradores/funcionarios.tsx
@@ -111,6 +111,7 @@ const EmployeeList = () => {
                                 value={searchQuery}
                                 onChange={(e) => setSearchQuery(e.target.value)}
                                 className="pl-4 pr-10 py-2.5 w-full border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                maxLength={11}
                             />
                             <div className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
                                 <FaSearch className="text-gray-400" />
