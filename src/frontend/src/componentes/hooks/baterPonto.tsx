49d64fd6a0c1debee5715b563800476a641c4837
victordanielrb <victordanielramosb@gmail.com>
2025-04-26 15:05:09 -0300

    fix: correção de erro após merge

--- src/frontend/src/componentes/hooks/baterPonto.tsx
+++ src/frontend/src/componentes/hooks/baterPonto.tsx
@@ -1,7 +1,8 @@
 import api from "./api";
 import recarregar from "./hooksChamarBackend/recarregar";
+import refetch from "./hooksChamarBackend/refetch";
 
-export default async function baterPonto() { 
+export default async function baterPonto(setDados) { 
     const userData = JSON.parse(localStorage.getItem('user') || '{}');
     const id_colaborador = userData.id_colaborador; 
 
@@ -9,8 +10,8 @@ export default async function baterPonto() {
         
         const response = await api.post('/turno/bater-ponto');
         //fetch nos dados apos bater turno
-       
-        recarregar();
+        let dados = await refetch();
+        setDados(dados);
         
         console.log('Ponto batido com sucesso:', response.data);
         
