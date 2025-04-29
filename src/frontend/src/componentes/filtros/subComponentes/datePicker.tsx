6b3e6f2ca9f5fc87202b5a877bed2988ca0fc158
victordanielrb <victordanielramosb@gmail.com>
2025-04-29 20:51:05 -0300

    fix: corrige bug na responsividade

--- src/frontend/src/componentes/filtros/subComponentes/datePicker.tsx
+++ src/frontend/src/componentes/filtros/subComponentes/datePicker.tsx
@@ -52,7 +52,7 @@ export default function DateRangePicker({
                     endDate={endDate}
                     minDate={startDate instanceof Date ? startDate : undefined}
                     placeholderText="Data final"
-                    className="pl-16 sm:pl-10 pr-4 py-2.5 w-full border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all duration-200 shadow-sm"
+                    className="pl-16 sm:pl-10 pr-10 py-2.5 w-full border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all duration-200 shadow-sm"
                     dateFormat="dd/MM/yyyy"
                     onKeyDown={(e) => e.preventDefault()}
                     showMonthDropdown
